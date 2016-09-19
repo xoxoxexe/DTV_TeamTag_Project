89392916205ea48d446cb6f9835d18bcd9e89c64
Matthias <Matthias@FUJITSULIFEBOOK>
2016-09-19 17:31:10 +0200

    - Added Angebot, Position - Added IAngebotFacade

---
+++ DAL.Contracts/Backend.Contracts/Angebot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backend.Contracts
+{
+    public class Angebot
+    {
+        public int Id { get; set; }
+
+        public int KundeId { get; set; }
+
+        public IEnumerable<Position> Positionen { get; set; }
+
+        public double Gesamt { get; set; }
+
+        public DateTime Datum { get; set; }
+    }
+}
--- DAL.Contracts/Backend.Contracts/Kunde.cs
+++ DAL.Contracts/Backend.Contracts/Kunde.cs
@@ -3,6 +3,8 @@ namespace Backend.Contracts
 {
     public class Kunde
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public string Email { get; set; }
