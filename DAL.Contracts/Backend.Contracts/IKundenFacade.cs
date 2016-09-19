923bf34caf0984d22cbd735ea1d180bcb6bf69f3
Matthias <Matthias@FUJITSULIFEBOOK>
2016-09-19 17:40:17 +0200

    GetKunden in Contracts hinzugefuegt

--- DAL.Contracts/Backend.Contracts/IKundenFacade.cs
+++ DAL.Contracts/Backend.Contracts/IKundenFacade.cs
@@ -1,7 +1,11 @@
-﻿namespace Backend.Contracts
+﻿using System.Collections.Generic;
+
+namespace Backend.Contracts
 {
     public interface IKundenFacade
     {
         void Save(Kunde kunde);
+
+        IEnumerable<Kunde> GetKunden();
     }
 }
