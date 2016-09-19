3ed4f1560342cd7f59202f19efdfc896a5330180
Matthias <Matthias@FUJITSULIFEBOOK>
2016-09-19 18:40:00 +0200

    Datenbankzugriff für KundenFacade implementiert

---
+++ DAL.Contracts/BackendImpl.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Backend.Contracts;
+using Backend.Impl;
+
+namespace BackendImpl.Test
+{
+    [TestClass]
+    public class KundenFacadeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IKundenFacade facade = new KundenFacade();
+            var kunden = facade.GetKunden();
+            var bla = 0;
+
+        }
+    }
+}
