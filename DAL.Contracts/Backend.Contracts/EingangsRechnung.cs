160badd89412b1ce93cd0d9b513a5b201ac590e6
MnWiegel <MnWiegel@SI161009>
2016-09-20 13:23:37 +0200

    Rechnungen Facades

---
+++ DAL.Contracts/Backend.Contracts/EingangsRechnung.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Contracts
+{
+    public class EingangsRechnung
+    {
+
+        public DateTime Datum { get; set; }
+        public string Nummer { get; set; }
+        public double Preis { get; set; }
+
+        public string LieferantName { get; set; }
+    }
+}
---
+++ DAL.Contracts/Backend.Impl/FakeEingangsRechnungFacade.cs
@@ -0,0 +1,30 @@
+﻿using Backend.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Impl
+{
+    public class FakeEingangsRechnungFacade : IEingangsRechnungFacade
+    {
+        public IEnumerable<EingangsRechnung> GetEingangsRechnungen()
+        {
+            return new List<EingangsRechnung> {
+               new EingangsRechnung {
+                    Datum = new DateTime(2016,9,1),
+                    LieferantName = "Döner um die Ecke",
+                    Nummer = "2",                
+                    Preis = 3.50   
+               },
+               new EingangsRechnung {
+                    Datum = new DateTime(2016,8,2),
+                    LieferantName = "Portal Entwiclungskosten",
+                    Nummer = "1",
+                    Preis = 50000
+               }
+           };
+        }
+    }
+}
