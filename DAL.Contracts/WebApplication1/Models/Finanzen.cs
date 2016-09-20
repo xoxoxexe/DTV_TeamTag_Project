0512905e19f216e8ab3e8bc37bb8a86c59125c97
Tim <Tim@ERAZER>
2016-09-20 15:00:09 +0200

    Jetz abba!

---
+++ DAL.Contracts/WebApplication1/Models/Finanzen.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Finanzen
+    {
+        public IEnumerable<FinanzenPosition> Rechnungen
+        {
+            get; set;
+        }
+
+        public decimal AktuellerSaldo
+        {
+            get; set;
+        }
+    }
+}
