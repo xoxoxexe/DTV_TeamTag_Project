98ecfd0c4bd410b9ba7522d140ee5c263edbacbf
Matthias <Matthias@FUJITSULIFEBOOK>
2016-09-20 09:44:02 +0200

    Kunde zurückgeben implementiert

--- DAL.Contracts/Backend.Impl/KundenFacade.cs
+++ DAL.Contracts/Backend.Impl/KundenFacade.cs
@@ -13,13 +13,34 @@ namespace Backend.Impl
             List<Kunde> listKunde = new List<Kunde>();
             using (var context = new teamtageEntities())
             {
-                foreach(Kunden kunde in context.Kunden)
+                foreach (Kunden kunde in context.Kunden)
                 {
                     listKunde.Add(new Kunde() { Email = kunde.Email, Id = kunde.ID, Name = kunde.Name, Ort = kunde.Ort, Plz = kunde.Plz, Strasse = kunde.Strasse, Telefon = kunde.Tel });
-                }                
+                }
             }
 
-            return listKunde;            
+            return listKunde;
+        }
+
+        public Kunde GetKunde(Guid guid)
+        {
+            Kunde mappedKunde;
+            using (var context = new teamtageEntities())
+            {
+                Kunden kunde = context.Kunden.Find(guid);
+
+                mappedKunde = new Kunde()
+                {
+                    Email = kunde.Email,
+                    Id = kunde.ID,
+                    Name = kunde.Name,
+                    Ort = kunde.Ort,
+                    Plz = kunde.Plz,
+                    Strasse = kunde.Strasse,
+                    Telefon = kunde.Tel
+                };
+            }
+            return mappedKunde;
         }
 
         void IKundenFacade.Save(Kunde kunde)
