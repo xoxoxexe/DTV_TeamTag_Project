578c56624bf06b2fb2cebef1d0b4c4ebee89eb62
MnWiegel <MnWiegel@SI161009>
2016-09-20 11:42:48 +0200

    Rechnungen hinzugefügt

--- DAL.Impl/Rechnungen.cs
+++ DAL.Impl/Rechnungen.cs
@@ -12,27 +12,22 @@ namespace DAL.Impl
     using System;
     using System.Collections.Generic;
     
-    public partial class Kunden
+    public partial class Rechnungen
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Kunden()
+        public Rechnungen()
         {
-            this.Angebote = new HashSet<Angebote>();
-            this.Rechnungen = new HashSet<Rechnungen>();
+            this.Rechnungspositionen = new HashSet<Rechnungspositionen>();
         }
     
-        public string Name { get; set; }
-        public string Plz { get; set; }
-        public string Strasse { get; set; }
-        public string HNr { get; set; }
-        public string Ort { get; set; }
-        public string Tel { get; set; }
-        public string Email { get; set; }
-        public System.Guid ID { get; set; }
+        public int Rechnungsnummer { get; set; }
+        public Nullable<System.DateTime> Erstellungsdatum { get; set; }
+        public string Betreff { get; set; }
+        public System.Guid KundeID { get; set; }
+        public bool Bezahlt { get; set; }
     
+        public virtual Kunden Kunden { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Angebote> Angebote { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Rechnungen> Rechnungen { get; set; }
+        public virtual ICollection<Rechnungspositionen> Rechnungspositionen { get; set; }
     }
 }
