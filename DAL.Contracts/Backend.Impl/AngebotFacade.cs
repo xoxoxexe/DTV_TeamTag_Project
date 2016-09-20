d4b732dc64c7538cb05b76f0255665593673dda7
Matthias <Matthias@FUJITSULIFEBOOK>
2016-09-20 11:56:18 +0200

    Create Update angepasst.

--- DAL.Contracts/Backend.Impl/AngebotFacade.cs
+++ DAL.Contracts/Backend.Impl/AngebotFacade.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using AutoMapper;
 using Backend.Contracts;
 using DAL.Impl;
 
@@ -11,6 +11,7 @@ namespace Backend.Impl
         IEnumerable<Angebot> IAngebotFacade.GetAngebote()
         {
 
+
             //return new List<Angebot>()
             //{
             //    new Angebot() {AngebotsNummer = 1, Betreff = "SUper Betreff", Datum =  DateTime.Now, Gesamt =  1000, Positionen = new List<Position>() {new Position() {Freitext = "Hier ist freier Text", Preis = 1000} } },
@@ -18,16 +19,18 @@ namespace Backend.Impl
             //    new Angebot() {AngebotsNummer = 3, Betreff = "Super Betreff", Datum =  DateTime.Now, Gesamt =  1000, Positionen = new List<Position>() {new Position() {Freitext = "Hier ist freier Text", Preis = 1000} } }
             //};
 
-
-
-
-
             List<Angebot> allAngebote = new List<Angebot>();
             using (var context = new teamtageEntities1())
             {
+                //var mapper = config.CreateMapper();
+
                 foreach (Angebote angebot in context.Angebote)
                 {
                     allAngebote.Add(new Angebot() { Betreff = angebot.Betreff, Datum = angebot.Datum, Id = angebot.ID, KundeId = angebot.KundeID, Positionen = MapDalAngebotsPositionenToBackend(angebot.Angebotspositionen), Gesamt = GetAngebotGesamtpreis(angebot.Angebotspositionen) });
+                    //allAngebote.Add(mapper.Map(angebot, new Angebot()));
+
+                    //Angebot mappedAngebot = mapper.Map<Angebot>(angebot);
+                    //allAngebote.Add(mappedAngebot);
                 }
             }
             return allAngebote;
@@ -35,10 +38,9 @@ namespace Backend.Impl
 
         void IAngebotFacade.Save(Angebot angebot)
         {
-
             using (var context = new teamtageEntities1())
             {
-                context.Angebote.Add(new Angebote() {  Angebotspositionen = MapBackendAngebotsPositionenToDal(angebot.Positionen), Betreff = angebot.Betreff, Datum = angebot.Datum, KundeID = angebot.KundeId, ID = angebot.Id });
+                context.Angebote.Add(new Angebote() {Angebosnummer = angebot.AngebotsNummer ,Angebotspositionen = MapBackendAngebotsPositionenToDal(angebot.Positionen), Betreff = angebot.Betreff, Datum = angebot.Datum, KundeID = angebot.KundeId });
                 context.SaveChanges();
             }
         }
@@ -46,6 +48,11 @@ namespace Backend.Impl
         private ICollection<Position> MapDalAngebotsPositionenToBackend(ICollection<Angebotspositionen> angebotsPositionen)
         {
             List<Position> mappedBackendPositionen = new List<Position>();
+            if (angebotsPositionen == null)
+            {
+                return mappedBackendPositionen;
+            }
+            
             foreach (Angebotspositionen positionen in angebotsPositionen)
             {
                 mappedBackendPositionen.Add(new Position { Freitext = positionen.Freitext, Preis = (double)positionen.Reis });
@@ -53,9 +60,14 @@ namespace Backend.Impl
             return mappedBackendPositionen;
         }
 
-        private ICollection<Angebotspositionen> MapBackendAngebotsPositionenToDal(IEnumerable<Position> angebotsPositionen)
+        public static ICollection<Angebotspositionen> MapBackendAngebotsPositionenToDal(IEnumerable<Position> angebotsPositionen)
         {
-            List<DAL.Impl.Angebotspositionen> mappedDalPositionen = new List<Angebotspositionen>();
+            List<Angebotspositionen> mappedDalPositionen = new List<Angebotspositionen>();
+
+            if (angebotsPositionen == null)
+                return mappedDalPositionen;
+
+            
             foreach (Position positionen in angebotsPositionen)
             {
                 mappedDalPositionen.Add(new Angebotspositionen { Freitext = positionen.Freitext, Reis = (decimal)positionen.Preis });
@@ -63,14 +75,13 @@ namespace Backend.Impl
             return mappedDalPositionen;
         }
 
-        private double GetAngebotGesamtpreis(ICollection<Angebotspositionen> angebotsPositionen)
+        public static double GetAngebotGesamtpreis(ICollection<Angebotspositionen> angebotsPositionen)
         {
             double sum = 0;
             foreach (Angebotspositionen position in angebotsPositionen)
             {
                 sum += (double)position.Reis;
             }
-
             return sum;
         }
     }
--- DAL.Contracts/Backend.Impl/KundenFacade.cs
+++ DAL.Contracts/Backend.Impl/KundenFacade.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity.Migrations;
 using Backend.Contracts;
 using DAL.Impl;
-using System.Linq;
 
 namespace Backend.Impl
 {
@@ -11,16 +9,12 @@ namespace Backend.Impl
     {
         IEnumerable<Kunde> IKundenFacade.GetKunden()
         {
-
             //return new List<Kunde>()
             //{
             //    new Kunde() {Email = "teamdatev@datev.de", Name = "DatevTEam", Ort = "Nürnberg", Plz = "91522", Telefon = "0911/18383", Strasse = "PaumgartnerStraße"},
             //    new Kunde() {Email = "teasdasdastev@datev.de", Name = "DatevTEdasdasdasdasdasam", Ort = "Nürnberg", Plz = "91522", Telefon = "0911/18383", Strasse = "PaumgartnerStraße"},
             //    new Kunde() {Email = "teamdatev@datev.de", Name = "DatevTEam", Ort = "Nürnberg", Plz = "91522", Telefon = "0911/18383", Strasse = "PaumgartnerStraße"},
             //};
-
-
-
 
             List<Kunde> listKunde = new List<Kunde>();
             using (var context = new teamtageEntities1())
@@ -55,8 +49,6 @@ namespace Backend.Impl
             //    Strasse = "PaumgartnerStraße"
             //};
 
-
-
             Kunde mappedKunde;
             using (var context = new teamtageEntities1())
             {
@@ -80,15 +72,35 @@ namespace Backend.Impl
         {
             using (var context = new teamtageEntities1())
             {
-                Kunden foundKunde = context.Kunden.Find(kunde);
-                foundKunde.Email = kunde.Email;
-                foundKunde.ID = kunde.Id;
-                foundKunde.Name = kunde.Name;
-                foundKunde.Ort = kunde.Ort;
-                foundKunde.Strasse = kunde.Strasse;
-                foundKunde.Plz = kunde.Plz;
-                foundKunde.Tel = kunde.Telefon;
+                Kunden foundKunde = context.Kunden.Find(kunde.Id);
+                if (foundKunde != null)
+                {
+                    foundKunde.Email = kunde.Email;
+                    foundKunde.ID = kunde.Id;
+                    foundKunde.Name = kunde.Name;
+                    foundKunde.Ort = kunde.Ort;
+                    foundKunde.Strasse = kunde.Strasse;
+                    foundKunde.Plz = kunde.Plz;
+                    foundKunde.Tel = kunde.Telefon;
+                }
+                else
+                {
+                    Kunden neuerKunde = new Kunden()
+                    {
+                        Email = kunde.Email,
+                        ID = Guid.NewGuid(),
+                        Name = kunde.Name,
+                        Ort = kunde.Ort,
+                        Strasse = kunde.Strasse,
+                        Plz = kunde.Plz,
+                        Tel = kunde.Telefon 
+                    };
+
+                    context.Kunden.Add(neuerKunde);
+                }
+
                 context.SaveChanges();
+
             }
         }
     }
